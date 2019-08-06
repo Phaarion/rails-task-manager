9ffb5d1301a262c3246627eefa00fb77b8673c2f
marion phare <marion.phare@wanadoo.fr>
2019-08-06 16:24:04 +0200

    task valid

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   get    'tasks',          to: 'tasks#index'
 
   get    'tasks/new',      to: 'tasks#new'
@@ -9,8 +10,8 @@ Rails.application.routes.draw do
 
   get    'tasks/:id',      to: 'tasks#show', as: :task
 
-  get    'tasks/:id/edit', to: 'tasks#edit'
+  get    'tasks/:id/edit', to: 'tasks#edit', as: :edit
   patch  'tasks/:id',      to: 'tasks#update'
 
-  delete 'tasks/:id',      to: 'tasks#destroy'
+  delete 'tasks/:id',      to: 'tasks#destroy', as: :delete
 end
