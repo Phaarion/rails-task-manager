2c98fe08897ece33734afabb274f2801e5306268
marion phare <marion.phare@wanadoo.fr>
2019-08-06 14:50:53 +0200

    Task links ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
-Rails.application.routes.draw do
-  get 'task', to: 'pages#task', as: :task
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    'tasks',          to: 'tasks#index'
+
+  get    'tasks/new',      to: 'tasks#new'
+  post   'tasks',          to: 'tasks#create'
+
+  get    'tasks/:id',      to: 'tasks#show', as: :task
+
+  get    'tasks/:id/edit', to: 'tasks#edit'
+  patch  'tasks/:id',      to: 'tasks#update'
+
+  delete 'tasks/:id',      to: 'tasks#destroy'
 end
